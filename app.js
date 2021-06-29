5babcbad99a4cd5361c1a5852b945beda48b5e45
muhammedrishadpe <rishadpe@gmail.com>
2021-06-29 10:31:58 +0530

    addToBookList

--- app.js
+++ app.js
@@ -45,7 +45,25 @@ class UI {
 // Store Class: Handle Storage
 
 // Event: Display Books
-document.addEventListener('DOMContentLoaded', UI.displayBooks)
+document.addEventListener('DOMContentLoaded', UI.displayBooks);
 // Event: Add a book
+document.querySelector('#book-form').addEventListener('submit', (e)=> {
+
+    // Prevent actual submit
+    e.preventDefault();
+
+    //Get form values
+    const title = document.querySelector('#title').value;
+    const author = document.querySelector('#author').value;
+    const isbn = document.querySelector('#isbn').value;
+
+    //Instatiate book
+    const book = new Book(title, author, isbn);
+
+    //Add Book to UI
+    UI.addBookToList(book);
+
+
+});
 
 // Event: Remove a Book
