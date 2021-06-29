207d982be8459577b70988721e46a9191f93660f
muhammedrishadpe <rishadpe@gmail.com>
2021-06-29 12:07:58 +0530

    Updated more funcations

--- app.js
+++ app.js
@@ -48,6 +48,17 @@ class UI {
         }
     }
 
+    static showAlert(message, className) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+        //Vanish in 3 seconds
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
+    }
+
     static clearFields() {
         document.querySelector('#title').value = ''
         document.querySelector('#author').value = ''
@@ -69,15 +80,26 @@ document.querySelector('#book-form').addEventListener('submit', (e)=> {
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
 
-    //Instatiate book
-    const book = new Book(title, author, isbn);
+    // Validate
+    if(title === '' || author === '' || isbn === '') {
+        UI.showAlert('Please fill in all fields', 'danger');
+    } else {
 
-    //Add Book to UI
-    UI.addBookToList(book);
+        //Instatiate book
+        const book = new Book(title, author, isbn);
 
-    //Clear Fields
-    UI.clearFields();
+        //Add Book to UI
+        UI.addBookToList(book);
 
+        // Show sw=uccess message
+        UI.showAlert('Book Added', 'success');
+
+        //Clear Fields
+        UI.clearFields();
+
+    }
+    
+   
 });
 
 // Event: Remove a Book
